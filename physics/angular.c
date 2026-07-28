9bb88225cc3c6488a5b2d7267da41de8e2994f62
Harshit-Dhanwalkar <harshitpd1729@gmail.com>
2026-07-28 12:40:56 +0530

    Add Gaunt-coefficient formula in angular for HF p/d subshell implementation

--- physics/angular.c
+++ physics/angular.c
@@ -19,20 +19,27 @@ complex_t sigma_z[4] = {{1.0, 0.0}, {0.0, 0.0}, {0.0, 0.0}, {-1.0, 0.0}};
 
 // Ladder operators
 complex_t l_plus_op(int l, int m, int m_prime) {
-  if (m_prime != m + 1)
+  if (m_prime != m + 1) {
     return c_zero();
-  if (m < -l || m >= l)
+  }
+  if (m < -l || m >= l) {
     return c_zero();
+  }
+
   double val = sqrt((double)(l - m) * (l + m + 1));
+
   return c_real(val);
 }
 
 complex_t l_minus_op(int l, int m, int m_prime) {
-  if (m_prime != m - 1)
+  if (m_prime != m - 1) {
     return c_zero();
-  if (m <= -l || m > l)
+  }
+  if (m <= -l || m > l) {
     return c_zero();
+  }
   double val = sqrt((double)(l + m) * (l - m + 1));
+
   return c_real(val);
 }
 
@@ -40,48 +47,58 @@ complex_t l_minus_op(int l, int m, int m_prime) {
 cmatrix_t *l2_matrix(int l) {
   int dim = 2 * l + 1;
   cmatrix_t *M = cmatrix_alloc(dim, dim);
-  if (!M)
+  if (!M) {
     return NULL;
+  }
+
   for (int i = 0; i < dim; i++) {
     CMAT(M, i, i) = c_real(l * (l + 1.0));
   }
+
   return M;
 }
 
 cmatrix_t *lz_matrix(int l) {
   int dim = 2 * l + 1;
   cmatrix_t *M = cmatrix_alloc(dim, dim);
-  if (!M)
+  if (!M) {
     return NULL;
+  }
   for (int i = 0; i < dim; i++) {
     int m = i - l;
     CMAT(M, i, i) = c_real(m);
   }
+
   return M;
 }
 
 cmatrix_t *lx_matrix(int l) {
   int dim = 2 * l + 1;
   cmatrix_t *M = cmatrix_alloc(dim, dim);
-  if (!M)
+  if (!M) {
     return NULL;
+  }
+
   for (int i = 0; i < dim; i++) {
     int m = i - l;
-    // <m+1|Lx|m> = 0.5 * sqrt((l-m)(l+m+1))
+    // <m+1|Lx|m> = 0.5 * \sqrt((l-m)(l+m+1))
     if (m < l) {
       double val = 0.5 * sqrt((double)(l - m) * (l + m + 1));
       CMAT(M, i + 1, i) = c_real(val);
       CMAT(M, i, i + 1) = c_real(val);
     }
   }
+
   return M;
 }
 
 cmatrix_t *ly_matrix(int l) {
   int dim = 2 * l + 1;
   cmatrix_t *M = cmatrix_alloc(dim, dim);
-  if (!M)
+  if (!M) {
     return NULL;
+  }
+
   for (int i = 0; i < dim; i++) {
     int m = i - l;
     if (m < l) {
@@ -90,6 +107,7 @@ cmatrix_t *ly_matrix(int l) {
       CMAT(M, i, i + 1) = c_imag(val);
     }
   }
+
   return M;
 }
 
@@ -98,17 +116,20 @@ static double lfact(int n) { return lgamma((double)n + 1.0); }
 // Clebsch-Gordan coefficient via Racah's formula.
 double clebsch_gordan(int j1_2, int m1_2, int j2_2, int m2_2, int J_2,
                       int M_2) {
-  if (m1_2 + m2_2 != M_2)
+  if (m1_2 + m2_2 != M_2) {
     return 0.0;
-  if (J_2 < abs(j1_2 - j2_2) || J_2 > j1_2 + j2_2)
+  }
+  if (J_2 < abs(j1_2 - j2_2) || J_2 > j1_2 + j2_2) {
     return 0.0;
-  if (abs(m1_2) > j1_2 || abs(m2_2) > j2_2 || abs(M_2) > J_2)
+  }
+  if (abs(m1_2) > j1_2 || abs(m2_2) > j2_2 || abs(M_2) > J_2) {
     return 0.0;
-  if ((j1_2 + j2_2 + J_2) % 2 != 0)
+  }
+  if ((j1_2 + j2_2 + J_2) % 2 != 0) {
     return 0.0;
+  }
 
   int j1 = j1_2, m1 = m1_2, j2 = j2_2, m2 = m2_2, J = J_2, M = M_2;
-
   double log_pref =
       0.5 * (log((double)(J + 1)) + lfact((J + j1 - j2) / 2) +
              lfact((J - j1 + j2) / 2) + lfact((j1 + j2 - J) / 2) -
@@ -131,8 +152,10 @@ double clebsch_gordan(int j1_2, int m1_2, int j2_2, int m2_2, int J_2,
           lfact((j2 + m2) / 2 - k) + lfact((J - j2 + m1) / 2 + k) +
           lfact((J - j1 - m2) / 2 + k));
     double term = exp(log_term);
-    if (k % 2 != 0)
+
+    if (k % 2 != 0) {
       term = -term;
+    }
     sum += term;
   }
 
@@ -145,8 +168,10 @@ int couple_allowed_J(int j1_2, int j2_2, int *J2_out) {
   int count = 0;
 
   for (int J = Jmin; J <= Jmax; J += 2) {
-    if (J2_out)
+    if (J2_out) {
       J2_out[count] = J;
+    }
+
     count++;
   }
 
@@ -154,24 +179,30 @@ int couple_allowed_J(int j1_2, int j2_2, int *J2_out) {
 }
 
 cvector_t *couple_states(int j1_2, int j2_2, int J_2, int M_2) {
-  if (j1_2 < 0 || j2_2 < 0)
+  if (j1_2 < 0 || j2_2 < 0) {
     return NULL;
-  if (J_2 < abs(j1_2 - j2_2) || J_2 > j1_2 + j2_2)
+  }
+  if (J_2 < abs(j1_2 - j2_2) || J_2 > j1_2 + j2_2) {
     return NULL;
-  if (abs(M_2) > J_2)
+  }
+  if (abs(M_2) > J_2) {
     return NULL;
-  if ((j1_2 + j2_2 + J_2) % 2 != 0)
+  }
+  if ((j1_2 + j2_2 + J_2) % 2 != 0) {
     return NULL;
+  }
 
   int dim1 = j1_2 + 1; // = 2*j1+1, whether j1 integer or half-integer
   int dim2 = j2_2 + 1;
 
   cvector_t *v = cvector_alloc(dim1 * dim2);
-  if (!v)
+  if (!v) {
     return NULL;
+  }
 
-  for (int i = 0; i < dim1 * dim2; i++)
+  for (int i = 0; i < dim1 * dim2; i++) {
     v->data[i] = c_zero();
+  }
 
   // Same index convention as lz_matrix: index i -> m = i - j, so m1_2 = -j1_2 +
   // 2*i1 and m2_2 = -j2_2 + 2*i2.
@@ -179,8 +210,9 @@ cvector_t *couple_states(int j1_2, int j2_2, int J_2, int M_2) {
     int m1_2 = -j1_2 + 2 * i1;
     for (int i2 = 0; i2 < dim2; i2++) {
       int m2_2 = -j2_2 + 2 * i2;
-      if (m1_2 + m2_2 != M_2)
-        continue; // CG is exactly 0 here; leave the slot at c_zero()
+      if (m1_2 + m2_2 != M_2) {
+        continue; // CG is exactly 0 here; leave slot at c_zero()
+      }
 
       double cg = clebsch_gordan(j1_2, m1_2, j2_2, m2_2, J_2, M_2);
       v->data[i1 * dim2 + i2] = c_real(cg);
@@ -190,26 +222,46 @@ cvector_t *couple_states(int j1_2, int j2_2, int J_2, int M_2) {
   return v;
 }
 
-// Spin operations
+// Gaunt coefficient
+double gaunt_coefficient(int l, int m, int k, int lp, int mp) {
+  int q = m - mp;
+
+  double cg_triangle = clebsch_gordan(2 * l, 0, 2 * k, 0, 2 * lp, 0);
+  double cg_m = clebsch_gordan(2 * l, -2 * m, 2 * k, 2 * q, 2 * lp, -2 * mp);
+
+  // Parity check via bitwise AND: negative operands under two's-complement
+  // representation, unlike (m-mp) % 2.
+  double sign = ((m - mp) & 1) ? -1.0 : 1.0;
+
+  return sign * sqrt((2.0 * l + 1.0) / (2.0 * lp + 1.0)) * cg_triangle * cg_m;
+}
+
+/* Spin operations */
 void spin_sigma_x(cvector_t *spinor) {
-  if (!spinor || spinor->n != 2)
+  if (!spinor || spinor->n != 2) {
     return;
+  }
+
   complex_t a = spinor->data[0];
   spinor->data[0] = spinor->data[1];
   spinor->data[1] = a;
 }
 
 void spin_sigma_y(cvector_t *spinor) {
-  if (!spinor || spinor->n != 2)
+  if (!spinor || spinor->n != 2) {
     return;
+  }
+
   complex_t a = spinor->data[0];
   spinor->data[0] = c_mul(c_imag(-1.0), spinor->data[1]);
   spinor->data[1] = c_mul(c_imag(1.0), a);
 }
 
 void spin_sigma_z(cvector_t *spinor) {
-  if (!spinor || spinor->n != 2)
+  if (!spinor || spinor->n != 2) {
     return;
+  }
+
   spinor->data[0] = spinor->data[0];
   spinor->data[1] = c_scale(spinor->data[1], -1.0);
 }
--- physics/angular.h
+++ physics/angular.h
@@ -5,17 +5,17 @@
 #include "../core/matrix.h"
 #include "../core/vector.h"
 
-// Pauli matrices (2x2)
+/* Pauli matrices (2x2) */
 extern complex_t sigma_x[4]; // row-major, indexed as [i*2 + j]
 extern complex_t sigma_y[4];
 extern complex_t sigma_z[4];
 
-// Ladder operators for angular momentum: L_{\pm} = L_x \pm i L_y
+/* Ladder operators for angular momentum: L_{\pm} = L_x \pm i L_y */
 complex_t l_plus_op(int l, int m,
                     int m_prime); // matrix element <l,m+1|L_+|l,m>
 complex_t l_minus_op(int l, int m, int m_prime); // <l,m-1|L_-|l,m>
 
-// Matrix representation of L^2, L_z for a given l (dimension 2l+1)
+/* Matrix representation of L^2, L_z for a given l (dimension 2l+1) */
 cmatrix_t *l2_matrix(int l);
 cmatrix_t *lz_matrix(int l);
 cmatrix_t *lx_matrix(int l);
@@ -47,6 +47,19 @@ cvector_t *couple_states(int j1_2, int j2_2, int J_2, int M_2);
  * J_2 = |j1_2-j2_2|, |j1_2-j2_2|+2, ..., j1_2+j2_2.
  */
 int couple_allowed_J(int j1_2, int j2_2, int *J2_out);
+
+/*
+ * Gaunt coefficient c^k(l,m; l',m') = \sqrt(4 * \pi / (2k+1)) * \int Y_lm^*
+ * Y_kq Y_l'm' d\Omega, q = m - m'. Built from clebsch_gordan() above via
+ *   c^k(l,m;l',m') = (-1)^(m-m') * sqrt((2l+1)/(2l'+1))
+ *                    * <l0;k0|l'0> * <l,-m;k,q|l',-m'>
+ * l, m, k, l', m' are integers, since orbital angular momenta are always
+ * integers.
+ *
+ * Returns 0 if triangle inequality |l-l'| <= k <= l+l' is violated, if l+k+l'
+ * is odd or if |m|>l or |m'|>l'.
+ */
+double gaunt_coefficient(int l, int m, int k, int lp, int mp);
 
 // Spin-1/2 operations: apply \sigma_x, \sigma_y, \sigma_z to 2-component spinor
 void spin_sigma_x(cvector_t *spinor);
